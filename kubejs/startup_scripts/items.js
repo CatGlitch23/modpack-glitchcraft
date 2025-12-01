39cc1ace13bae4145c327ea61d571aa12d1b0ed8
CatGlitch_ <106968244+CatGlitch23@users.noreply.github.com>
2025-12-01 12:17:30 -0400

    Create sword item with custom attributes
    Added a new sword item with specific properties.

--- kubejs/startup_scripts/items.js
+++ kubejs/startup_scripts/items.js
@@ -1,1 +1,9 @@
+StartupEvents.registry('item', event => {  
+    event.create('sword')  
+        .texture('mod_sword_texture')  
+        .attackDamage(8.0)  
+        .attackSpeed(-2.0)  
+        .tooltip('Esta espada es solo para moderadores') // Add tooltip  
+        .glow(true) // Add glow effect  
+})
 
