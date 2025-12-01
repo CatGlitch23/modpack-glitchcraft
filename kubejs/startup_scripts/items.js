a2197d4d9e47343a1b021d563c06ec02457c25d3
CatGlitch_ <106968244+CatGlitch23@users.noreply.github.com>
2025-12-01 19:44:41 -0400

    Update display and tooltip text for sword_mod_custom

--- kubejs/startup_scripts/items.js
+++ kubejs/startup_scripts/items.js
@@ -3,8 +3,8 @@ StartupEvents.registry("item", (event) => {
     .create("sword_mod_custom", "sword")
     .attackDamageBaseline(10)
     .speedBaseline(1.6)
-    .displayName("Espada de Moderador")
+    .displayName("§aEspada de Moderador")
     .glow(true)
     .maxStackSize(1)
-    .tooltip("Una espada especial para moderadores.");
+    .tooltip("§eUna espada especial para moderadores.");
 });
