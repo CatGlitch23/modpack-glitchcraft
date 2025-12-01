c8305bc0bc79f246c7c422c9273ad96b1eadbe46
CatGlitch_ <106968244+CatGlitch23@users.noreply.github.com>
2025-12-01 11:57:16 -0400

    Remove custom sword item registration

--- kubejs/startup_scripts/items.js
+++ kubejs/startup_scripts/items.js
@@ -1,8 +1,1 @@
-StartupEvents.registry('item', event => {  
-    event.create('mod:sword', 'sword')  
-        .texture('custom_sword_texture')  
-        .attackDamage(8.0)  
-        .attackSpeed(-2.0)  
-        .tooltip('Esta espada es solo para moderadores') // Add tooltip  
-        .glow(true) // Add glow effect  
-})
+
